df68e22b751b80a598e37b88fce3aef4a05bd563
Ryan Liu <rliu@medrio.com>
2021-02-17 12:48:30 -0800

    Test shows  Don’t return IEnumerable<> uniess you have to

--- CoreWebApp1/CoreWebApp1/Program.cs
+++ CoreWebApp1/CoreWebApp1/Program.cs
@@ -1,26 +1,20 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CoreWebApp1
 {
-  public class Program
+    public class Program
   {
     public static void Main(string[] args)
     {
-      CreateHostBuilder(args).Build().Run();
+        Host.CreateDefaultBuilder(args) //=> IHostBuilder
+            .ConfigureWebHostDefaults(webBuilder =>     // => IHostBuilder
+            {
+                webBuilder.UseStartup<Startup>();
+            })
+            .Build()  // => IHost
+            .Run();  //  =>  void
     }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-              webBuilder.UseStartup<Startup>();
-            });
   }
 }
