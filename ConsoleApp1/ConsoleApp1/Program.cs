04c25648aa2c9f8e13ea91fba099e22613d29c4c
Ryan Liu <rliu@medrio.com>
2021-06-22 11:43:37 -0700

    Add ioc infrastructure

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,17 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using ConsoleApp1.Validation;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
 namespace ConsoleApp1
 {
     class Program
     {
+        internal static ServiceProvider Container;
+
         static void Main(string[] args)
         {
+            OneTimeSetup();
             //TestDateTime.Test();
             TestValidation.Test();
         }
+
         
+        private static void OneTimeSetup()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            var services = new ServiceCollection();
+            
+            services.AddLogging(configure => configure.AddDebug());
+            services.AddLogging(configure => configure.AddSimpleConsole(c =>
+            {
+                c.ColorBehavior = LoggerColorBehavior.Enabled;
+                c.UseUtcTimestamp = false;
+                c.TimestampFormat = "[yy-MM-dd HH:mm:ss]";
+            }));
+
+            Container = services.BuildServiceProvider();
+            
+
+        }
     }
 }
 
