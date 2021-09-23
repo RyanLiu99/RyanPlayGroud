be14604cbf124c07d9dbfb2df82375333738aa88
Ryan Liu <rliu@medrio.com>
2021-09-23 15:19:16 -0700

    Console log works, but GCP logging is not working yet. No error, but nothing to to GCP

--- GcpLoggingCoreMvc/GcpConsoleLog4Net21/Program.cs
+++ GcpLoggingCoreMvc/GcpConsoleLog4Net21/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using Google.Cloud.Logging.Log4Net;
+using log4net;
 
 namespace GcpConsoleLog4Net21
 {
+    //PS  $env:GOOGLE_APPLICATION_CREDENTIALS = "C:\rliu-OneDrive\OneDrive\IT\Google service account key when generate it ask to save  ryantest1-4fd63-447661f46186.json"
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+           var c = log4net.Config.XmlConfigurator.Configure();
+
+            ILog log = LogManager.GetLogger(typeof(Program));
+            log.Info("Gcp logging from log4net in console app.");
+
+            LogManager.Flush(50);
         }
     }
 }
