bd599e50c24055a63dbd3221a46c56967e8cfe96
Ryan Liu <rliu@medrio.com>
2021-12-13 15:21:51 -0800

    env and app name

--- GcpLoggingCoreMvc/GcpLoggingCoreMvc/Controllers/HomeController.cs
+++ GcpLoggingCoreMvc/GcpLoggingCoreMvc/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using GcpLoggingNet5MvcLogDirectlyAndILogger.Models;
 using GcpLoggingNet5MvcLogDirectlyAndILogger.Services;
 using Medrio.ActivityAuditLog;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace GcpLoggingNet5MvcLogDirectlyAndILogger.Controllers
@@ -16,8 +17,11 @@ namespace GcpLoggingNet5MvcLogDirectlyAndILogger.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly IMyService _myService;
         private readonly IActivityAuditLogger _activityAuditLogger;
+        private readonly IHostEnvironment _hostEnvironment;
 
-        public HomeController(ILogger<HomeController> logger, IMyService myService, IActivityAuditLogger activityAuditLogger)
+        public HomeController(ILogger<HomeController> logger, IMyService myService, 
+            IActivityAuditLogger activityAuditLogger,
+            IHostEnvironment hostEnvironment)
         {
             if (myService is null)
             {
@@ -27,6 +31,7 @@ namespace GcpLoggingNet5MvcLogDirectlyAndILogger.Controllers
             _logger = logger;
             _myService = myService;
             _activityAuditLogger = activityAuditLogger;
+            _hostEnvironment = hostEnvironment;
         }
 
         public IActionResult Index()
@@ -34,15 +39,18 @@ namespace GcpLoggingNet5MvcLogDirectlyAndILogger.Controllers
             return View();
         }
 
-        public Task<string> Log()
+        public async Task<string> Log()
         {
-            return _activityAuditLogger.WriteLog(this.HttpContext);
+            var result = await _activityAuditLogger.WriteLog(this.HttpContext);
+            return result
+                   + " | App : " + _hostEnvironment.ApplicationName  //GcpLoggingNet5MvcLogDirectlyAndILogger 
+                   + " | Env : " + _hostEnvironment.EnvironmentName; //Development
 
             //var response = new DirectGcpLogging().WriteLog(this.HttpContext);   
             //_logger.LogCritical(TestGcpLoggingEventId, new Exception("Fake exception"), "2 HomeController ILogger log a CriticalMsg: {criticalMsg}", new CriticalMsg { Age = 55, CriticalStr = "Prop2"});
             ////_logger.LogInformation("In Controller, Activity.Current?.Id is {activityId}, HttpContextTraceId is {traceId}", Activity.Current?.Id, HttpContext.TraceIdentifier);
             //_myService.WriteSomeLog();
-            
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
