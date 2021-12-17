ac625023c6b14050b73a432f1d60f1dbd9f35051
Ryan Liu <rliu@medrio.com>
2021-12-17 13:31:20 -0800

    webapiNet5 project init commit , intent use it test k8s

---
+++ WebApiNet5/WebApiNet5/Controllers/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiNet5.Controllers
+{
+    [ApiController]
+    [Route("/[action]")]
+    public class DefaultController
+    {
+        private readonly ILogger<DefaultController> logger;
+
+        public DefaultController(ILogger<DefaultController> logger)
+        {
+            this.logger = logger;
+        }
+        [HttpGet]
+        public object Index()
+        {
+            var s = $"Receive a request at  + {DateTime.Now}.  \r\n" +
+                    $"Go to <a href='/swagger/'>/swagger/</a>";
+            logger.LogInformation(s);
+            return s;
+        }
+    }
+}
