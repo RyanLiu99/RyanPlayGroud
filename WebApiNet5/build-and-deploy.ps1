d25c0a7b3b59ccc6e372dc3602a4490e5886b5db
Ryan Liu <rliu@medrio.com>
2021-12-18 00:35:34 -0800

    docker works perfectly now

--- WebApiNet5/build-and-deploy.ps1
+++ WebApiNet5/build-and-deploy.ps1
@@ -1,9 +1,10 @@
 $OldLocaton = Get-Location
 Set-Location "$PSSCRIPTROOT\docker"
 docker-compose stop
-docker ps --format "{{.Image}}#{{.ID}}" -n 1 -q | where {$_ -Match "webapinet5"} | foreach-object {docker rm -f $_.split("#")[1]}
-docker rm -fv WebApiNet5
+docker ps --format "{{.Image}}#{{.ID}}" -n 1 -q | Where-Object {$_ -Match "webapinet5"} | foreach-object {docker rm -f $_.split("#")[1]}  
+# docker container rm -fv WebApiNet5
 docker system prune -f
 docker-compose up -d --build
-[System.Diagnostics.Process]::Start("https://localhost:12777/Index")
+try { [System.Diagnostics.Process]::Start("https://localhost:152/Index")  # PS 5 only
+} catch { } #Ignore
 Set-Location $OldLocaton
