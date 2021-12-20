d271f021497316db732ea0ec636c8ec19016fa7a
Ryan Liu <rliu@medrio.com>
2021-12-19 22:28:32 -0800

    portname is useless. to start chrome, need in admin mode

--- WebApiNet5-DockerK8s/k8s/deployment-ingress-version/applyk8s.ps1
+++ WebApiNet5-DockerK8s/k8s/deployment-ingress-version/applyk8s.ps1
@@ -2,5 +2,5 @@ kubectl apply -f WebApiNet5.yaml
 kubectl apply -f WebApiNet5-service.yaml
 kubectl apply -f WebApiNet5-ingress.yaml
 
-try { [System.Diagnostics.Process]::Start("http://webapinet5.local.medrio.com/")  # PS 5 only
+try { [System.Diagnostics.Process]::Start("http://webapinet5.local.medrio.com/?q=afda")  # PS 5 only, admin mode
 } catch { } #Ignore
