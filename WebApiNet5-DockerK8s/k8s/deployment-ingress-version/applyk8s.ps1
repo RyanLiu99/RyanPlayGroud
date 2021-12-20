c2ab7444c3068e31e77fc26b1460681f225d838d
Ryan Liu <rliu@medrio.com>
2021-12-19 22:25:18 -0800

    ingress version also works now

--- WebApiNet5-DockerK8s/k8s/deployment-ingress-version/applyk8s.ps1
+++ WebApiNet5-DockerK8s/k8s/deployment-ingress-version/applyk8s.ps1
@@ -2,5 +2,5 @@ kubectl apply -f WebApiNet5.yaml
 kubectl apply -f WebApiNet5-service.yaml
 kubectl apply -f WebApiNet5-ingress.yaml
 
-try { [System.Diagnostics.Process]::Start("http://localhost:99/")  # PS 5 only
+try { [System.Diagnostics.Process]::Start("http://webapinet5.local.medrio.com/")  # PS 5 only
 } catch { } #Ignore
