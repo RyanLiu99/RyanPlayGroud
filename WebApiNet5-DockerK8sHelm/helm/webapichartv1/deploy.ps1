9a4c248fb6798bd82e814e04c5c6cd5c677a0f85
Ryan Liu <rliu@medrio.com>
2021-12-20 11:59:17 -0800

    helm v1 works now, it is nothing, ust copied from k8s ingress version. both script will ensure ingress controller class less.

--- WebApiNet5-DockerK8sHelm/helm/webapichartv1/deploy.ps1
+++ WebApiNet5-DockerK8sHelm/helm/webapichartv1/deploy.ps1
@@ -1,8 +1,6 @@
 helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace --set controller.watchIngressWithoutClass=true
 
-kubectl apply -f WebApiNet5.yaml
-kubectl apply -f WebApiNet5-service.yaml
-kubectl apply -f WebApiNet5-ingress.yaml
+helm install webapichartv1 .
 
 try { [System.Diagnostics.Process]::Start("http://webapinet5.local.medrio.com/?q=afda")  # PS 5 only, admin mode
 } catch { } #Ignore
