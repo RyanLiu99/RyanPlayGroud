f6ab4ad115f79bd4a65384410041715951967b47
Ryan Liu <rliu@medrio.com>
2021-12-21 12:01:10 -0800

    use upgrade cmd instead of install. apply --namespace=ryanplay.  apply override full name

--- WebApiNet5-DockerK8sHelm/helm/webapichartv2dyanmic/deploy.ps1
+++ WebApiNet5-DockerK8sHelm/helm/webapichartv2dyanmic/deploy.ps1
@@ -1,6 +1,6 @@
-helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace --set controller.watchIngressWithoutClass=true
+# helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace --set controller.watchIngressWithoutClass=true
 
-helm install webapichartv2 .
+helm upgrade webapichartv2 . --install --create-namespace --namespace ryanplay
 
 try { [System.Diagnostics.Process]::Start("http://webapichartv2.local.medrio.com/?q=afda")  # PS 5 only, admin mode
 } catch { } #Ignore
