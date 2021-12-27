1f7d0ba5ebf7c7962cb1c2cec9f033d32633ed21
Ryan Liu <rliu@medrio.com>
2021-12-26 23:24:56 -0800

    can use var for image name and container name, but not container port

--- MicroServices/FrontEnd/docker/deploy.ps1
+++ MicroServices/FrontEnd/docker/deploy.ps1
@@ -5,11 +5,20 @@ function prune{
 }
 
 $OldLocaton = Get-Location
-Set-Location "$PSSCRIPTROOT\..\" #build above \src\ to access docker file in this folder
-docker build -t frontend -f docker\Dockerfile .  #docker file is relative to build context
-docker run -d -p 808:80 frontend
+$imageName = 'webapinet5/frontend'
+$containerName = 'webapifrontend'
+$exposePort=808
 
-try { [System.Diagnostics.Process]::Start("http://localhost:808")  # PS 5 only
+Set-Location "$PSSCRIPTROOT\..\" #build above \src\ to access docker file in this folder
+
+docker build -t $imageName -f docker\Dockerfile .  #docker file is relative to build context
+
+#delete old container
+docker ps --format "{{.Image}}#{{.ID}}" -n 1 -q | Where-Object {$_ -Match $containerName} | foreach-object {docker rm -f $_.split("#")[1]}  
+
+docker run -it -d -p 808:80 --restart=unless-stopped --name=$containerName $imageName 
+
+try { [System.Diagnostics.Process]::Start("http://localhost:$exposePort")  # PS 5 only
 } catch { } #Ignore
 
 Set-Location $OldLocaton
