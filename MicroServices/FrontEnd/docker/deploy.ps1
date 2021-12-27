ccf8ee1fb1aa98eab4d1bab48bb07dba1c8f8b63
Ryan Liu <rliu@medrio.com>
2021-12-26 23:39:29 -0800

    $port in powershell still not work but add trap to show error ...

--- MicroServices/FrontEnd/docker/deploy.ps1
+++ MicroServices/FrontEnd/docker/deploy.ps1
@@ -1,3 +1,7 @@
+trap {
+    "Error found:  $_"
+    exit 1
+}
 function prune{
     docker container prune -f
     docker image prune -f
@@ -14,10 +18,13 @@ Set-Location "$PSSCRIPTROOT\..\" #build above \src\ to access docker file in thi
 docker build -t $imageName -f docker\Dockerfile .  #docker file is relative to build context
 
 #delete old container
-docker ps --format "{{.Image}}#{{.ID}}" -n 1 -q | Where-Object {$_ -Match $containerName} | foreach-object {docker rm -f $_.split("#")[1]}  
+Write-Host 'Delete containers by image ...'
+docker ps --format "{{.Image}}#{{.ID}}" -n 1 -q | Where-Object {$_ -Match $imageName} | foreach-object {docker rm -f $_.split("#")[1]}  
 
-docker run -it -d -p 808:80 --restart=unless-stopped --name=$containerName $imageName 
+Write-Host 'Create docker container ...'
+docker run -it -d -p 808:80 --restart=unless-stopped --name=$containerName $imageName  #$($exposePort):80 won't work
 
+Write-Host 'Start brower (need in admin mode) ...'
 try { [System.Diagnostics.Process]::Start("http://localhost:$exposePort")  # PS 5 only
 } catch { } #Ignore
 
