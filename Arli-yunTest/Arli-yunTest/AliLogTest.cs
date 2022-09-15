e4e9b37eddde871ada0d3b48e1753faef2deb0e0
Ryan Liu <rliu@medrio.com>
2022-09-15 12:28:07 -0700

    Created and install aliyun package

---
+++ Arli-yunTest/Arli-yunTest/AliLogTest.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using System;
+
+namespace Arli_yunTest
+{
+    public class Tests
+    {
+    
+
+        [SetUp]
+        public void Setup()
+        {
+          
+        }
+
+
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
