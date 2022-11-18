2a08b97da5658a4a5713d6bc7ba36d11be84c99c
Ryan Liu <rliu@medrio.com>
2022-11-18 11:05:41 -0800

    init Cachelib

---
+++ CacheLib/CacheLib/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace CacheLib
+{
+    public class Class1
+    {
+        public Class1(IMemoryCache memoryCache)
+        {
+            memoryCache.Set<object>(new object(), new object());
+
+        }
+    }
+}
