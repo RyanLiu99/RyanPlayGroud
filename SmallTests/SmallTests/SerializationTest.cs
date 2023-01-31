98590811094c8f4f7d87a1a6b190653e9987debb
Ryan Liu <rliu@medrio.com>
2023-01-31 15:30:00 -0800

    Tuple works as ValueTuple

--- SmallTests/SmallTests/SerializationTest.cs
+++ SmallTests/SmallTests/SerializationTest.cs
@@ -33,7 +33,7 @@ namespace SmallTests
             TestSerializationSimple(WireUsingMessagePack);
         }
 
-  
+
         private void TestSerializationSimple(Func<ValueDependencies, ValueDependencies> transform)
         {
             var entityDependency = new EntityDependency<Person, int>(1, 1, 2, 2);
@@ -114,6 +114,45 @@ namespace SmallTests
         }
 
         #endregion TestValueTuple
+
+        #region TestTuple
+        [Test]
+        public void TestSerializationTupleMessagepack()
+        {
+            TestSerializationTuple(WireUsingMessagePack);
+        }
+
+        private void TestSerializationTuple(Func<ValueDependencies, ValueDependencies> transform)
+        {
+            var entityDependency = new EntityDependency<Person, Tuple<int, string>>(
+                new Tuple<int, string>(1, "K1"),
+                new Tuple<int, string>(1, "K1"),
+                new Tuple<int, string>(2, "K1"),
+                new Tuple<int, string>(2, "K2")
+            );
+
+            var collectionDependency = new string[] { "Col1", "Col1", "Coll2" };
+
+            var valueDependencies = new ValueDependencies(new[] { entityDependency }, collectionDependency);
+
+            var deserialized = transform(valueDependencies);
+
+            Assert.IsNotNull(deserialized);
+            Assert.AreEqual(valueDependencies.CollectionDependencies.Count, deserialized.CollectionDependencies.Count);
+            Assert.AreEqual(valueDependencies.EntityDependencies[0].Ids.Count, deserialized.EntityDependencies[0].Ids.Count);
+
+
+            var compressed = deserialized.Compress();
+            Assert.AreEqual(2, compressed.CollectionDependencies.Count); //one duplicate "Col1" removed
+
+
+            var entityDepd = compressed?.EntityDependencies?[0];
+            Assert.NotNull(entityDepd);
+            Assert.AreEqual(typeof(Person).FullName, entityDepd.EntityTypeName);
+
+            Assert.AreEqual(3, entityDepd.Ids.Count); //  2x {1, K1}, (2, K1), (2, K2). For NewtonJson, Duplicate is not removed yet, it is JObject.
+        }
+        #endregion
 
         #region transform funcs
 
