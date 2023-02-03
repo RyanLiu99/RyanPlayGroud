dddcbec10b4d5ee8f8ed8bc4528bf9e40b5f4ad9
Ryan Liu <rliu@medrio.com>
2023-02-02 23:08:38 -0800

    Better way to set up unit test use host, so configuration can be applied to log providers etc

--- SmallTests/SmallTests/GlobalSetup.cs
+++ SmallTests/SmallTests/GlobalSetup.cs
@@ -1,44 +1,82 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Hosting;
 using NUnit.Framework;
+using System;
+using Microsoft.Extensions.Logging;
 
 namespace SmallTests
 {
     [SetUpFixture]
     public static class GlobalSetup
     {
-        internal static ServiceProvider? Container;
+        internal static IServiceProvider? Container;
+
+        //[OneTimeSetUp]
+        //public static void OneTimeSetup()
+        //{
+        //    IConfiguration configuration = new ConfigurationBuilder()
+        //        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        //        .Build();
+
+        //    var services = new ServiceCollection();
+        //    //services.AutoRegisterServices(configuration);
+
+        //    services.AddLogging(configure => configure.AddDebug());
+        //    services.AddLogging(configure => configure.AddSimpleConsole(c =>
+        //    {
+        //        c.ColorBehavior = LoggerColorBehavior.Enabled;
+        //        c.UseUtcTimestamp = false;
+        //        c.TimestampFormat = "[yy-MM-dd HH:mm:ss]";
+        //    }));
+
+        //    services.AddMemoryCache(options => options.TrackStatistics = true);
+        //    Container = services.BuildServiceProvider();
+        //    //Container.SetUpIocAdapter();
+        //}
+
 
         [OneTimeSetUp]
-        public static void OneTimeSetup()
+        public static void OneTimeSetup() //cannot have parameter
         {
-            IConfiguration configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .Build();
+            OneTimeSetupInternal(null);
+        }
 
-            var services = new ServiceCollection();
-            //services.AutoRegisterServices(configuration);
-
-            services.AddLogging(configure => configure.AddDebug());
-            services.AddLogging(configure => configure.AddSimpleConsole(c =>
-            {
-                c.ColorBehavior = LoggerColorBehavior.Enabled;
-                c.UseUtcTimestamp = false;
-                c.TimestampFormat = "[yy-MM-dd HH:mm:ss]";
-            }));
-
-            services.AddMemoryCache(options => options.TrackStatistics = true);
-            Container = services.BuildServiceProvider();
+        private static void OneTimeSetupInternal(Action<IServiceCollection, IConfiguration>? configureDelegate = null)
+        {
+            var host = CreateDefaultBuilder(configureDelegate).Build();
+            Container = host.Services;
             //Container.SetUpIocAdapter();
         }
 
         [OneTimeTearDown]
         public static void OneTimeTearDown()
         {
-            Container?.Dispose();
+            (Container as ServiceProvider)?.Dispose();
         }
+
+        static IHostBuilder CreateDefaultBuilder(Action<IServiceCollection, IConfiguration>? configureDelegate = null)
+        {
+            return Host.CreateDefaultBuilder()
+                .ConfigureLogging((context, loggingBuilder) =>
+                {
+                    loggingBuilder.ClearProviders();
+                    loggingBuilder.AddConsole();
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    ConfigureServices(services, context.Configuration);
+                    configureDelegate?.Invoke(services, context.Configuration);
+                });
+        }
+
+        private static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            
+            //services.AutoRegisterServices(configuration);
+            services.AddMemoryCache(options => options.TrackStatistics = true);
+        }
+
     }
 }
 
