3482845e9524e7123fa7187c8d760da7940a4d2a
Ryan Liu <rliu@medrio.com>
2023-02-07 09:47:01 -0800

    result

--- SmallTests/SmallTests/TryCatchTest.cs
+++ SmallTests/SmallTests/TryCatchTest.cs
@@ -11,6 +11,19 @@ namespace SmallTests
 {
     internal class TryCatchTest
     {
+        /*  Result:
+
+            info: TestHelpers[0]
+      	            Inner Try block
+            info: TestHelpers[0]
+      	            Inner Caught exception, before rethrow
+            info: TestHelpers[0]
+      	            Inner Finally
+            info: TestHelpers[0]
+                  Outer Caught ex again
+            info: TestHelpers[0]
+                  Outer Done testing         
+         */
         [Test]
         public void TestTry()
         {
