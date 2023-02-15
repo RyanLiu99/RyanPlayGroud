722dc5e14dbd1fcdd21fe25f7e5ceb7929084274
Ryan Liu <rliu@medrio.com>
2023-02-14 19:46:32 -0800

    BetterCancellationTokenTests passes

---
+++ SmallTests/SmallTests/BetterCancellationTokenTests.cs
@@ -0,0 +1,266 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SmallTests
+{
+    internal class BetterCancellationTokenTests
+    {
+        // assignedToTask, theOneCallsCancel, checkAndThrowIfCancellationRequestedOn, waitTask, expectedTaskStatus , taskWillStop
+
+        [Test]
+        public void TestCT1([Values(true, false)] bool waitTask)
+        {
+            using CancellationTokenSource cs1 = new CancellationTokenSource();
+            using CancellationTokenSource cs2 = new CancellationTokenSource();
+            using CancellationTokenSource csBoth = CancellationTokenSource.CreateLinkedTokenSource(cs1.Token, cs2.Token);
+            Test(cs1.Token, cs1, csBoth.Token, waitTask, TaskStatus.Canceled, true);
+         
+        }
+
+        [Test]
+        public void TestCT2([Values(true, false)] bool waitTask)
+        {
+            using CancellationTokenSource cs1 = new CancellationTokenSource();
+            using CancellationTokenSource cs2 = new CancellationTokenSource();
+            using CancellationTokenSource csBoth = CancellationTokenSource.CreateLinkedTokenSource(cs1.Token, cs2.Token);
+            Test(cs1.Token, cs2, csBoth.Token, waitTask, TaskStatus.Faulted, true);
+        }
+
+        [Test]
+        public void TestCT3([Values(true, false)] bool waitTask)
+        {
+            using CancellationTokenSource cs1 = new CancellationTokenSource();
+            using CancellationTokenSource cs2 = new CancellationTokenSource();
+            using CancellationTokenSource csBoth = CancellationTokenSource.CreateLinkedTokenSource(cs1.Token, cs2.Token);
+            Test(csBoth.Token, cs1, csBoth.Token, waitTask, TaskStatus.Canceled, true);
+        }
+        [Test]
+        public void TestCT4([Values(true, false)] bool waitTask)
+        {
+            using CancellationTokenSource cs1 = new CancellationTokenSource();
+            using CancellationTokenSource cs2 = new CancellationTokenSource();
+            using CancellationTokenSource csBoth = CancellationTokenSource.CreateLinkedTokenSource(cs1.Token, cs2.Token);
+            Test(csBoth.Token, cs2, csBoth.Token, waitTask, TaskStatus.Canceled, true);
+        }
+        
+        [Test]
+        public void TestCT5([Values(true, false)] bool waitTask)
+        {
+            using CancellationTokenSource cs1 = new CancellationTokenSource();
+            using CancellationTokenSource cs2 = new CancellationTokenSource();
+            using CancellationTokenSource csBoth = CancellationTokenSource.CreateLinkedTokenSource(cs1.Token, cs2.Token);
+            Test(csBoth.Token, cs1, cs1.Token, waitTask, TaskStatus.Canceled, true);
+        }
+
+        //[Test]
+        //public void TestCT6([Values(true, false)] bool waitTask)
+        //{
+        //    using CancellationTokenSource cs1 = new CancellationTokenSource();
+        //    using CancellationTokenSource cs2 = new CancellationTokenSource();
+        //    using CancellationTokenSource csBoth = CancellationTokenSource.CreateLinkedTokenSource(cs1.Token, cs2.Token);
+        //    Test(csBoth.Token, cs1, cs1.Token, waitTask, TaskStatus.Running, false);
+        //}
+
+        //=====================
+        [Test]
+        public async Task TestCTAsync1([Values(true, false)] bool waitTask)
+        {
+            using CancellationTokenSource cs1 = new CancellationTokenSource();
+            using CancellationTokenSource cs2 = new CancellationTokenSource();
+            using CancellationTokenSource csBoth = CancellationTokenSource.CreateLinkedTokenSource(cs1.Token, cs2.Token);
+            await TestAsync(csBoth.Token, cs1, cs2.Token, waitTask, TaskStatus.Canceled,true);
+        }
+
+        [Test]
+        public async Task TestCTAsync2([Values(true, false)] bool waitTask)
+        {
+            using CancellationTokenSource cs1 = new CancellationTokenSource();
+            using CancellationTokenSource cs2 = new CancellationTokenSource();
+            using CancellationTokenSource csBoth = CancellationTokenSource.CreateLinkedTokenSource(cs1.Token, cs2.Token);
+            await TestAsync(cs1.Token, cs2, csBoth.Token, waitTask, TaskStatus.Faulted, true);
+        }
+
+        [Test]
+        public async Task TestCTAsync3([Values(true, false)] bool waitTask)
+        {
+            using CancellationTokenSource cs1 = new CancellationTokenSource();
+            using CancellationTokenSource cs2 = new CancellationTokenSource();
+            using CancellationTokenSource csBoth = CancellationTokenSource.CreateLinkedTokenSource(cs1.Token, cs2.Token);
+            await TestAsync(csBoth.Token, cs1, csBoth.Token, waitTask, TaskStatus.Canceled, true);
+        }
+
+        [Test]
+        public async Task TestCTAsync4([Values(true, false)] bool waitTask)
+        {
+            using CancellationTokenSource cs1 = new CancellationTokenSource();
+            using CancellationTokenSource cs2 = new CancellationTokenSource();
+            using CancellationTokenSource csBoth = CancellationTokenSource.CreateLinkedTokenSource(cs1.Token, cs2.Token);
+            await TestAsync(csBoth.Token, cs2, csBoth.Token, waitTask, TaskStatus.Canceled, true);
+        }
+
+        [Test]
+        public async Task TestCTAsync5([Values(true, false)] bool waitTask)
+        {
+            using CancellationTokenSource cs1 = new CancellationTokenSource();
+            using CancellationTokenSource cs2 = new CancellationTokenSource();
+            using CancellationTokenSource csBoth = CancellationTokenSource.CreateLinkedTokenSource(cs1.Token, cs2.Token);
+            await TestAsync(csBoth.Token, cs1, cs1.Token, waitTask, TaskStatus.Canceled, true);
+        }
+
+        public void Test(
+            CancellationToken assignedToTask,
+            CancellationTokenSource theOneCallsCancel,
+            CancellationToken checkAndThrowIfCancellationRequestedOn,
+            bool waitTask,
+            TaskStatus expectedTaskStatus ,
+            bool taskWillStop
+        )
+        {
+            bool expectIsFaulted = expectedTaskStatus == TaskStatus.Faulted;
+            Exception e = null;
+            Task task = null;
+
+            try
+            {
+                task = Task.Factory.StartNew((state) =>
+                {
+                    int i = 0;
+                    while (i++ < 100)
+                    {
+                        Thread.Sleep(5);
+                        ((CancellationToken)state).ThrowIfCancellationRequested();
+                    }
+
+                }, checkAndThrowIfCancellationRequestedOn, assignedToTask);
+                // token task is on can be csBoth or cs1, no difference
+
+                theOneCallsCancel.Cancel(false);
+
+                if (waitTask)
+                {
+                    Assert.IsTrue(task.Wait(600), "Timed out"); //However, if the calling thread is not waiting on the task, this specific exception will not be propagated. 
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Log :Test 2 Exception occurred : {e}"); //since waited , here it got TaskCanceledException which is subclass of OperationCanceledException
+                e = ex;
+            }
+
+            Assert.AreEqual(taskWillStop, task.ContinueWith((t) =>
+            {
+                Assert.AreEqual(expectIsFaulted, t.IsFaulted);
+                Assert.AreEqual(expectedTaskStatus, t.Status);
+                if (expectIsFaulted)
+                {
+                    Assert.IsNotNull(t.Exception);
+                    Assert.IsTrue(t.Exception is AggregateException);
+                    Assert.IsTrue(t.Exception.GetBaseException() is OperationCanceledException);
+                    Assert.IsFalse(t.Exception.GetBaseException() is TaskCanceledException);
+                }
+            }).Wait(600), "timeout on continue");
+
+
+            //Thread.Sleep(20); //More then sleep in while
+
+            Assert.AreEqual(waitTask, e != null); // if waited,there is exception. If not waited, there is no exception. (even task in on different token, this is same as task on right token)
+            if (waitTask)
+            {
+                Assert.IsTrue(e is AggregateException); //strange, 
+                Assert.IsTrue(e.GetBaseException() is OperationCanceledException);
+            }
+
+            //No matter waited or not waited, task is in same state:
+            Assert.AreEqual(expectIsFaulted, task.IsFaulted);
+            Assert.AreEqual(expectedTaskStatus, task.Status);
+            if (expectIsFaulted)
+            {
+                Assert.IsNotNull(task.Exception);
+                Assert.IsTrue(task.Exception is AggregateException);
+                Assert.IsTrue(task.Exception.GetBaseException() is OperationCanceledException);
+                Assert.IsFalse(task.Exception.GetBaseException() is TaskCanceledException);
+            }
+        }
+        
+        public async Task TestAsync(
+            CancellationToken assignedToTask,
+            CancellationTokenSource theOneCallsCancel,
+            CancellationToken checkAndThrowIfCancellationRequestedOn,
+            bool waitTask,
+            TaskStatus expectedTaskStatus,
+            bool taskWillStop
+            )
+        {
+            bool expectIsFaulted = expectedTaskStatus == TaskStatus.Faulted;
+
+            Exception e = null;
+            Task task = null;
+
+            try
+            {
+                task = Task.Factory.StartNew((state) =>
+                {
+                    int i = 0;
+                    while (i++ < 100)
+                    {
+                        Thread.Sleep(5);
+                        ((CancellationToken)state).ThrowIfCancellationRequested();
+                    }
+
+                }, checkAndThrowIfCancellationRequestedOn, assignedToTask);
+                // token task is on can be csBoth or cs1, no difference
+
+                theOneCallsCancel.Cancel(false);
+
+                if (waitTask)
+                {
+                    Assert.IsTrue(task.Wait(600), "Timed out"); //However, if the calling thread is not waiting on the task, this specific exception will not be propagated. 
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Log :Test 2 Exception occurred : {e}"); //since waited , here it got TaskCanceledException which is subclass of OperationCanceledException
+                e = ex;
+            }
+
+            await task.ContinueWith((t) =>
+            {
+                Assert.AreEqual(expectIsFaulted, t.IsFaulted);
+                Assert.AreEqual(expectedTaskStatus, t.Status);
+                if (expectIsFaulted)
+                {
+                    Assert.IsNotNull(t.Exception);
+                    Assert.IsTrue(t.Exception is AggregateException);
+                    Assert.IsTrue(t.Exception.GetBaseException() is OperationCanceledException);
+                    Assert.IsFalse(t.Exception.GetBaseException() is TaskCanceledException);
+                }
+            });
+
+
+            //Thread.Sleep(20); //More then sleep in while
+
+            Assert.AreEqual(waitTask, e != null); // if waited,there is exception. If not waited, there is no exception. (even task in on different token, this is same as task on right token)
+            if (waitTask)
+            {
+                Assert.IsTrue(e is AggregateException); //strange
+                Assert.IsTrue(e.GetBaseException() is OperationCanceledException);
+            }
+
+            //No matter waited or not waited, task is in same state:
+            Assert.AreEqual(expectIsFaulted, task.IsFaulted);
+            Assert.AreEqual(expectedTaskStatus, task.Status);
+            if (expectIsFaulted)
+            {
+                Assert.IsNotNull(task.Exception);
+                Assert.IsTrue(task.Exception is AggregateException);
+                Assert.IsTrue(task.Exception.GetBaseException() is OperationCanceledException);
+                Assert.IsFalse(task.Exception.GetBaseException() is TaskCanceledException);
+            }
+        }
+    }
+}
--- SmallTests/SmallTests/CancellationTokeTests.cs
+++ SmallTests/SmallTests/CancellationTokeTests.cs
@@ -7,6 +7,7 @@ namespace SmallTests
 {
     internal class CancellationTokeTests
     {
+
         //Exception caught or not solely depends on if task is awaited or not.  Nothing special for TaskCanceledException which is sub class or OperationCanceledException
         [Test]
         public async Task SameCancellationTokenLeadToCancelNotFaultedCauseExceptionCaughtDependsOrNotOnAWait([Values(true, false)] bool waitTask)
@@ -26,7 +27,7 @@ namespace SmallTests
                         ((CancellationToken)state).ThrowIfCancellationRequested();
                     }
 
-                }, cs1.Token, cs1.Token); //state to throw, cs token task on
+                }, cs1.Token, cs1.Token); //state to throw,  task assigned token
 
                 cs1.Cancel(false);
                 if (waitTask)
@@ -102,57 +103,7 @@ namespace SmallTests
 
 
         [Test]
-        public async Task TaskOnDifferentTokenCauseFaulted([Values(true, false)] bool waitTask)
-        {
-            using CancellationTokenSource cs1 = new CancellationTokenSource();
-            using CancellationTokenSource cs2 = new CancellationTokenSource();
-
-            CancellationTokenSource csBoth = CancellationTokenSource.CreateLinkedTokenSource(cs1.Token, cs2.Token);
-
-            Exception e = null;
-            Task task = null;
-
-            try
-            {
-
-                task = Task.Factory.StartNew((state) =>
-                {
-                    int i = 0;
-                    while (i++ < 100)
-                    {
-                        Thread.Sleep(5);
-                        ((CancellationToken)state).ThrowIfCancellationRequested();
-                    }
-
-                }, csBoth.Token, cs2.Token); //state to throw on, cs token task is on
-                // token task is on can be csBoth.Token or cs1.Token, no difference
-
-                cs1.Cancel(false);
-
-
-                if (waitTask)
-                {
-                    await task.ConfigureAwait(false); //However, if the calling thread is not waiting on the task, this specific exception will not be propagated. 
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Log :Test 1 Exception occurred : {e}"); //since awaited , here it got TaskCanceledException which is subclass of OperationCanceledException
-                e = ex;
-            }
-
-            Thread.Sleep(20); //More then sleep in while
-            Assert.AreEqual(waitTask, e != null); // if waited,there is exception. If not waited, there is no exception. (even task in on different token, this is same as task on right token)
-
-            //No matter waited or not waited, task is in same state:
-            Assert.AreEqual(true, task.IsFaulted, "Is Faulted?");
-            Assert.IsNotNull( task.Exception );
-            Assert.AreEqual(TaskStatus.Faulted, task.Status, "Task should be faulted.");
-        }
-        
-
-        [Test]
-        public async Task TaskOnDifferentTokenOnContinuanceAsFaulted([Values(true, false)] bool waitTask)
+        public async Task TaskOnDifferentChildThanChildCancelsWhenJointThrowsOnContinuanceAsFaulted([Values(true, false)] bool waitTask)
         {
             using CancellationTokenSource cs1 = new CancellationTokenSource();
             using CancellationTokenSource cs2 = new CancellationTokenSource();
@@ -174,7 +125,7 @@ namespace SmallTests
                     }
 
                 }, csBoth.Token, cs2.Token); //state to throw on, cs token task is on
-                // token task is on can be csBoth.Token or cs1.Token, no difference
+                // token task is on can be csBoth or cs1, no difference
 
                 cs1.Cancel(false);
 
@@ -194,6 +145,9 @@ namespace SmallTests
                 Assert.AreEqual(true, t.IsFaulted);
                 Assert.IsNotNull(t.Exception);
                 Assert.AreEqual(TaskStatus.Faulted, t.Status);
+                Assert.IsTrue(task.Exception is AggregateException);
+                Assert.IsTrue(task.Exception.GetBaseException() is OperationCanceledException);
+                Assert.IsFalse(task.Exception.GetBaseException() is TaskCanceledException);
             }).ConfigureAwait(false);
 
             Assert.IsTrue(cs1.IsCancellationRequested);
@@ -201,10 +155,14 @@ namespace SmallTests
 
             Thread.Sleep(20); //More then sleep in while
             Assert.AreEqual(waitTask, e != null); // if waited,there is exception. If not waited, there is no exception. (even task in on different token, this is same as task on right token)
+            if (waitTask)
+            {
+                Assert.IsTrue(e is OperationCanceledException); //strange here is OperationCanceledException
+            }
 
             //No matter waited or not waited, task is in same state:
             Assert.AreEqual(true, task.IsFaulted, "Is Faulted?");
-            Assert.IsNotNull( task.Exception);
+            Assert.IsNotNull(task.Exception);
             Assert.IsTrue(task.Exception is AggregateException);
             Assert.IsTrue(task.Exception.GetBaseException() is OperationCanceledException);
             Assert.IsFalse(task.Exception.GetBaseException() is TaskCanceledException);
@@ -213,6 +171,60 @@ namespace SmallTests
 
 
 
-    
+        [Test]
+        public async Task TaskOnDifferenChildThanTheChildCancelsCauseFaultedWhenThrowOnJointed2([Values(true, false)] bool waitTask)
+        {
+            using CancellationTokenSource cs1 = new CancellationTokenSource();
+            using CancellationTokenSource cs2 = new CancellationTokenSource();
+
+            CancellationTokenSource csBoth = CancellationTokenSource.CreateLinkedTokenSource(cs1.Token, cs2.Token);
+
+            Exception e = null;
+            Task task = null;
+
+            try
+            {
+
+                task = Task.Factory.StartNew((state) =>
+                {
+                    int i = 0;
+                    while (i++ < 100)
+                    {
+                        Thread.Sleep(5);
+                        ((CancellationToken)state).ThrowIfCancellationRequested();
+                    }
+
+                }, csBoth.Token, cs1.Token); //throws, task on
+                // token task is on can be csBoth.Token or cs1.Token, no difference
+
+                cs2.Cancel(false);
+
+
+                if (waitTask)
+                {
+                    await task.ConfigureAwait(false); //However, if the calling thread is not waiting on the task, this specific exception will not be propagated. 
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Log :Test 1 Exception occurred : {e}"); //since awaited , here it got TaskCanceledException which is subclass of OperationCanceledException
+                e = ex;
+            }
+
+            Thread.Sleep(20); //More then sleep in while
+            Assert.AreEqual(waitTask, e != null); // if waited,there is exception. If not waited, there is no exception. (even task in on different token, this is same as task on right token)
+
+            if (waitTask)
+            {
+                Assert.IsTrue(e is OperationCanceledException);
+            }
+
+            //No matter waited or not waited, task is in same state:
+            Assert.AreEqual(true, task.IsFaulted, "Is Faulted?");
+            Assert.IsNotNull(task.Exception);
+            Assert.AreEqual(TaskStatus.Faulted, task.Status, "Task should be faulted.");
+        }
+
+
     }
 }
