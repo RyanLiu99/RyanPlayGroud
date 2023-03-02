3765a077c9c56475735186780bdf3140cf59e9fc
Ryan Liu <rliu@medrio.com>
2023-03-02 12:18:59 -0800

    report csp violation to self, need run on and collect on https point on same site (guess can use cros)

---
+++ WebApplication1OnFramework/WebApplication1OnFramework/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebSockets;
+
+namespace WebApplication1OnFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<string> _reports = new List<string>();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+
+        public async Task<ActionResult> Report()
+        {
+            Request.InputStream.Seek(0, SeekOrigin.Begin);
+            using (var reader = new StreamReader(Request.InputStream)) 
+            {
+                var result = await reader.ReadToEndAsync();
+
+                _reports.Add(result);
+                return new EmptyResult();
+            }
+        }
+
+
+        public ActionResult Reports()
+        {
+            return new ContentResult() { Content = string.Join(" ;\r\n", _reports) };
+        }
+    }
+}
---
+++ WebApplication1OnFramework/WebApplication1OnFramework/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication1OnFramework.MvcApplication" Language="C#" %>
