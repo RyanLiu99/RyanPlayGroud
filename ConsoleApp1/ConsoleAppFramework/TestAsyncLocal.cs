9d55d5dcc9601abc79e566c61b78833c1e6cfa93
Ryan Liu <rliu@medrio.com>
2023-09-19 12:03:12 -0700

    async local

--- ConsoleApp1/ConsoleAppFramework/TestAsyncLocal.cs
+++ ConsoleApp1/ConsoleAppFramework/TestAsyncLocal.cs
@@ -5,39 +5,59 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppFramework
 {
+    //Prove each thread starts an "asynclocal storage", even no await used. 
+
     public class Holder
     {
-        private AsyncLocal<string> _local = new AsyncLocal<string>();
+        private  AsyncLocal<string> _localAsync = new AsyncLocal<string>();  //static or not does not make difference since Holder is singleton in test
+        
+         private string _local;
+
+         public string ValueLocalAsync
+         {
+             get => _localAsync.Value;
+             set => _localAsync.Value = value;
+         }
 
         public string Value
         {
-            get => _local.Value;
-            set => _local.Value = value;
+            get => _local;
+            set => _local = value;
         }
 
         public void Print()
         {
-          Console.WriteLine($"Current Thread {Thread.CurrentThread.Name}, Value : {Value}");
+          Console.WriteLine($"Current Thread {Thread.CurrentThread.Name}, AsyncLocal var : {ValueLocalAsync}; Simple var : {Value}.");
         }
     }
 
     internal class TestAsyncLocal
     {
-        private Holder holder = new Holder();
+        private readonly Holder _holder = new Holder(); //reused singleton instance
 
         internal void Test()
         {
-           var threads = Enumerable.Range(1, 100).Select(x =>
-                new Thread(() => { this.DoSth(x.ToString()); }) { Name = "T" + x });
+            Enumerable.Range(1, 100).Select(x =>
+                {
+                    var t = new Thread(() => { this.DoSth(x.ToString()); }) { Name = "T" + x };
+                    t.Start();
+                    return t;
+                }
+            ).ToArray();
 
-           Parallel.ForEach(threads, t => t.Start());
         }
 
         private void DoSth(string input)
         {
-            holder.Value = input;
-            holder.Print();
-            holder.Value = "--------" + input;
+            _holder.Value = input;
+            _holder.ValueLocalAsync = input;
+            // if sleep and use regular var, all print value will be 100. NO sleep, right value.
+            // if _local is string, not int, value all stepping each other, including  "-----x" when sleeping
+            Thread.Sleep(1); 
+            _holder.Print();
+
+            _holder.Value = "--------" + input;
+            _holder.ValueLocalAsync = "********" + input;
         }
     }
 }
---
+++ TraceMetricTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+    private static Meter m = new Meter("TraceMetricTest");
+    private static Counter<int> _count1 = m.CreateCounter<int>("Count 1");
+    static void Main()
+    {
+        _count1.Add(1);
+        
+
+        Console.WriteLine("Press Enter to exit...");
+        Console.ReadLine();
+    }
+
+
+    private static void Loop()
+    {
+        m.
+        while (true)
+        {
+
+        }
+
+    }
+}
