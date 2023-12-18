2ec6cb87ec7f62e7bcf8ff2e28d48d1e5f84eca9
Ryan Liu <rliu@medrio.com>
2023-12-18 12:09:24 -0800

    /dynamic string is not intered

---
+++ SmallTests/SmallTests/StringTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using SmallTests.Entities;
+
+namespace SmallTests
+{
+    internal class StringTests
+    {
+        [Test]
+        public void StringIsIntered()
+        {
+            string b = "B";
+            var ab = $"A {b}";
+            
+            Assert.IsTrue(string.IsInterned("c") != null);
+            
+            Assert.IsTrue(string.IsInterned(b) != null);
+
+            Assert.IsTrue(string.IsInterned(ab) == null); //dynamic string is not intered
+
+        }
+
+     
+    }
+}
